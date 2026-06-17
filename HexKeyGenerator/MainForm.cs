a2e2fd566712b2760704d51f49d11a7112743029
Rajagopal807 <101199988+Rajagopal807@users.noreply.github.com>
2026-06-17 18:33:59 +0530

    Fix: Implement InitializeComponent method in MainForm

--- HexKeyGenerator/MainForm.cs
+++ HexKeyGenerator/MainForm.cs
@@ -7,6 +7,9 @@ namespace HexKeyGenerator
     public partial class MainForm : Form
     {
         private Dictionary<string, string> companyKeyMappings;
+        private TextBox companyTextBox;
+        private TextBox keyTextBox;
+        private ListBox resultsListBox;
 
         public MainForm()
         {
@@ -14,80 +17,86 @@ namespace HexKeyGenerator
             companyKeyMappings = new Dictionary<string, string>();
         }
 
-        private void Form_Load(object sender, EventArgs e)
+        private void InitializeComponent()
         {
+            this.companyTextBox = new System.Windows.Forms.TextBox();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+
+            // Form properties
             this.Text = "Hex Key Generator - 256-bit";
             this.StartPosition = FormStartPosition.CenterScreen;
             this.Size = new System.Drawing.Size(700, 500);
+            this.BackColor = System.Drawing.SystemColors.Control;
 
-            // Company Name Label and TextBox
+            // Company Name Label
             Label companyLabel = new Label();
             companyLabel.Text = "Company Name:";
             companyLabel.Location = new System.Drawing.Point(20, 20);
             companyLabel.Width = 120;
+            companyLabel.AutoSize = false;
             this.Controls.Add(companyLabel);
 
-            TextBox companyTextBox = new TextBox();
-            companyTextBox.Name = "companyTextBox";
-            companyTextBox.Location = new System.Drawing.Point(150, 20);
-            companyTextBox.Width = 300;
-            this.Controls.Add(companyTextBox);
+            // Company Name TextBox
+            this.companyTextBox.Location = new System.Drawing.Point(150, 20);
+            this.companyTextBox.Width = 300;
+            this.Controls.Add(this.companyTextBox);
 
             // Generate Button
             Button generateButton = new Button();
             generateButton.Text = "Generate Key";
             generateButton.Location = new System.Drawing.Point(470, 20);
             generateButton.Width = 100;
-            generateButton.Click += (s, e1) => GenerateKeyClick(companyTextBox);
+            generateButton.Click += GenerateButton_Click;
             this.Controls.Add(generateButton);
 
-            // Generated Key Label and TextBox
+            // Generated Key Label
             Label keyLabel = new Label();
             keyLabel.Text = "256-bit Hex Key:";
             keyLabel.Location = new System.Drawing.Point(20, 70);
             keyLabel.Width = 120;
+            keyLabel.AutoSize = false;
             this.Controls.Add(keyLabel);
 
-            TextBox keyTextBox = new TextBox();
-            keyTextBox.Name = "keyTextBox";
-            keyTextBox.Location = new System.Drawing.Point(150, 70);
-            keyTextBox.Width = 500;
-            keyTextBox.ReadOnly = true;
-            keyTextBox.Font = new System.Drawing.Font("Courier New", 10);
-            this.Controls.Add(keyTextBox);
+            // Generated Key TextBox
+            this.keyTextBox.Location = new System.Drawing.Point(150, 70);
+            this.keyTextBox.Width = 500;
+            this.keyTextBox.ReadOnly = true;
+            this.keyTextBox.Font = new System.Drawing.Font("Courier New", 10);
+            this.Controls.Add(this.keyTextBox);
 
             // Copy Button
             Button copyButton = new Button();
             copyButton.Text = "Copy";
             copyButton.Location = new System.Drawing.Point(660, 70);
             copyButton.Width = 20;
-            copyButton.Click += (s, e1) => CopyToClipboard(keyTextBox);
+            copyButton.Click += CopyButton_Click;
             this.Controls.Add(copyButton);
 
-            // Results ListBox
+            // Results Label
             Label resultLabel = new Label();
             resultLabel.Text = "Company - Key Mappings:";
             resultLabel.Location = new System.Drawing.Point(20, 120);
             resultLabel.Width = 200;
+            resultLabel.AutoSize = false;
             this.Controls.Add(resultLabel);
 
-            ListBox resultsListBox = new ListBox();
-            resultsListBox.Name = "resultsListBox";
-            resultsListBox.Location = new System.Drawing.Point(20, 150);
-            resultsListBox.Width = 650;
-            resultsListBox.Height = 300;
-            this.Controls.Add(resultsListBox);
+            // Results ListBox
+            this.resultsListBox.Location = new System.Drawing.Point(20, 150);
+            this.resultsListBox.Width = 650;
+            this.resultsListBox.Height = 300;
+            this.Controls.Add(this.resultsListBox);
 
             // Clear Button
             Button clearButton = new Button();
             clearButton.Text = "Clear All";
             clearButton.Location = new System.Drawing.Point(20, 460);
             clearButton.Width = 100;
-            clearButton.Click += (s, e1) => ClearAll(companyTextBox, keyTextBox, resultsListBox);
+            clearButton.Click += ClearButton_Click;
             this.Controls.Add(clearButton);
         }
 
-        private void GenerateKeyClick(TextBox companyTextBox)
+        private void GenerateButton_Click(object sender, EventArgs e)
         {
             string companyName = companyTextBox.Text.Trim();
 
@@ -103,7 +112,6 @@ namespace HexKeyGenerator
                 string hexKey = result.Item2;
 
                 // Display the key
-                TextBox keyTextBox = (TextBox)this.Controls["keyTextBox"];
                 keyTextBox.Text = hexKey;
 
                 // Store in mappings
@@ -131,7 +139,6 @@ namespace HexKeyGenerator
 
         private void UpdateResultsList()
         {
-            ListBox resultsListBox = (ListBox)this.Controls["resultsListBox"];
             resultsListBox.Items.Clear();
 
             foreach (var mapping in companyKeyMappings)
@@ -141,7 +148,7 @@ namespace HexKeyGenerator
             }
         }
 
-        private void CopyToClipboard(TextBox keyTextBox)
+        private void CopyButton_Click(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(keyTextBox.Text))
             {
@@ -150,7 +157,7 @@ namespace HexKeyGenerator
             }
         }
 
-        private void ClearAll(TextBox companyTextBox, TextBox keyTextBox, ListBox resultsListBox)
+        private void ClearButton_Click(object sender, EventArgs e)
         {
             companyTextBox.Clear();
             keyTextBox.Clear();
